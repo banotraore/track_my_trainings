60b1223b36b42f970a10534a78e675ebaff609af
Bano Traore <47910631+banotraore@users.noreply.github.com>
2021-02-08 21:20:48 +0100

    Team model created and validations done

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_08_200725) do
+ActiveRecord::Schema.define(version: 2021_02_08_201229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "teams", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
